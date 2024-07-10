f8f9d8fc87542af71c18ce384514f19eb6a5303b
Itallo <ibenedetti91@hotmail.com>
2024-07-10 13:58:24 -0400

    add(main): enviroment for frontend connection

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { EnvironmentService } from './application/environment';
+
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const environmentService = app.get(EnvironmentService);
+  const port = environmentService.get('PORT') || 3000;
 
   app.setGlobalPrefix('api/v1');
 
@@ -15,6 +20,10 @@ async function bootstrap() {
     }),
   );
 
-  await app.listen(3000);
+  app.enableCors({
+    origin: 'http://localhost:5173',
+  });
+
+  await app.listen(port);
 }
 bootstrap();
